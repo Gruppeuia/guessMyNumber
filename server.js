9cd5d8dca2262003f8deea1607f910033c05bedf
Christian Simonsen <Christian@agens.no>
2017-09-27 15:00:22 +0200

    we got some server stuff runing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+var highscoreList = ["CHRISTIAN 1"];
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
@@ -13,6 +14,9 @@ app.get('/', function(req, res) {
   res.send(staticApp);
 });
 
+app.get('/highScore', function(req,res){
+    res.send(highscoreList);
+});
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
