c5906ad59a01963d06540238976fbabc06d16a35
Christian Simonsen <Christian@agens.no>
2017-10-04 14:48:21 +0200

    Now with database suport

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const url = require('url')
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const {Client} = require("pg");
 
-var highscoreList = [{name:"Christian", score:900, timestamp:"123456"}];
+var highscoreList = [];
+
+getHighScores();
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
@@ -24,6 +27,7 @@ app.post("/score", function(req,res){
     if(score.name != undefined && score.score != undefined){
         console.log(`Post score request ${score.name} ${score.score}`);
         highscoreList.push(score);
+        saveHighScores(score);
         res.status(200).end();
     } else{
         res.status(400).end();
@@ -39,3 +43,42 @@ app.use(function(err, req, res, next) {
 app.listen(app.get('port'), function() {
     console.log('Guess my number server running', app.get('port'));
 });
+
+
+function getHighScores(){
+    
+    let client = new Client({
+        connectionString:"postgres://hfekljdibcptzj:7b53232f8d744bc5c18e3f8ca05d2975a95946f96e3ddde0fc6fc725e0792357@ec2-176-34-242-58.eu-west-1.compute.amazonaws.com:5432/d4hur6e4168fvr",
+        ssl:true
+    });
+
+    client.connect();
+
+    client.query("select * from highscores", (err,resp) =>{
+        
+        console.dir(resp);
+        resp.rows.forEach(row => {
+            highscoreList.push(row);
+        });
+        client.end();
+    });
+
+}
+
+function saveHighScores(score){
+    
+    let client = new Client({
+        connectionString:"postgres://hfekljdibcptzj:7b53232f8d744bc5c18e3f8ca05d2975a95946f96e3ddde0fc6fc725e0792357@ec2-176-34-242-58.eu-west-1.compute.amazonaws.com:5432/d4hur6e4168fvr",
+        ssl:true
+    });
+
+    client.connect();
+
+    let sql = `INSERT INTO "highscores"("name", "score", "timestamp") VALUES('${score.name}', ${score.score}, '${score.timestamp}') RETURNING "id", "name", "score", "timestamp";`
+
+    client.query(sql, (err,resp) =>{
+        console.dir(resp);
+        client.end();
+    });
+
+}
